df234700f8b4ba3548e9ba47d4eaa06c83acbece
Banananaki <sephiroth_of_mana@yahoo.com>
2018-07-12 03:15:28 -0400

    v2 youtube fix

---
+++ snips/linksbar.php
@@ -0,0 +1,14 @@
+<?php
+echo '
+<nav class="navbar navbar-expand-lg fixed-bottom navbar-dark footer">
+    <div class="mx-auto">
+        <a href="http://facebook.com/banananaki"><img class="smalllogo" src="../images/fblogo.png"></a>
+        <a href="http://youtube.com/user/bananaki"> <img class="smalllogo" src="../images/ytlogo.png"></a>
+        <a href="http://github.com/banananaki"><img class="smalllogo" src="../images/githublogo.png" ></a>
+        <a href="http://soundcloud.com/banananaki"><img class="smalllogo" src="../images/sclogo.png"></a>
+        <a href="https://open.spotify.com/user/ovvsmu48bxi2r5zsz5hnm0weh?si=-4ncpeOPQSSpo3NCuF8zKA"><img class="smalllogo" src="../images/spotifylogo.png"></a>
+        <a href="http://twitch.tv/banananaki"><img class="smalllogo" src="../images/twitchlogo.png"></a>
+    </div>
+</nav>
+';
+?>
